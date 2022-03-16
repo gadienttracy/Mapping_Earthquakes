43a31148798eb52389edc60ffde6de63b46e8397
Gadient <Gadient.Tracy@mayo.edu>
2022-03-15 21:26:27 -0500

    logic.js file

--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -14,6 +14,14 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });
 
+
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//add circle marker to the map for LA
+L.circle([834.0522, -118.2437], {
+    radius: 100
+}).addTo(map);
 //Add a tile layer to map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
