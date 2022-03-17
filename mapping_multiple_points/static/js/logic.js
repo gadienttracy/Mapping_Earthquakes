5160d12eb5fa01efc866da51952871a3e5d4d84d
Gadient <Gadient.Tracy@mayo.edu>
2022-03-16 21:10:03 -0500

    mapping lines

--- mapping_multiple_points/static/js/logic.js
+++ mapping_multiple_points/static/js/logic.js
@@ -21,17 +21,16 @@ let cityData = cities;
 cityData.forEach(function(city) {
     console.log(city)
     L.circleMarker(city.location, {
-        radius: city.population
+        radius: city.population/100000
     })
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
   .addTo(map);
 });
 
-//Add a tile layer to map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -19,11 +19,19 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 //add circle marker to the map for LA
-L.circle([834.0522, -118.2437], {
-    radius: 100
+// L.circle([834.0522, -118.2437], {
+//     radius: 100
+// }).addTo(map);
+
+//circle marker with custom radius
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
 }).addTo(map);
-//Add a tile layer to map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//Add a tile layer that is the background to the map
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
